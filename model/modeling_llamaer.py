410a399ded6ed9c6f6fd47efbf6446a8366989ea
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2025-06-30 05:02:20 +0700

    u

--- model/modeling_llamaer.py
+++ model/modeling_llamaer.py
@@ -69,13 +69,13 @@ class EmbedPreceedLlamaModel(LlamaModel):
             )
 
         if position_ids is None:
-            # position_ids = cache_position.unsqueeze(0)
-            past_key_values_length = past_key_values.get_usable_length(seq_length)
-            device = input_ids.device if input_ids is not None else inputs_embeds.device
-            position_ids = torch.arange(
-                past_key_values_length, seq_length + past_key_values_length, dtype=torch.long, device=device
-            )
-            position_ids = position_ids.unsqueeze(0)
+            position_ids = cache_position.unsqueeze(0)
+            # past_key_values_length = past_key_values.get_usable_length(seq_length)
+            # device = input_ids.device if input_ids is not None else inputs_embeds.device
+            # position_ids = torch.arange(
+            #     past_key_values_length, seq_length + past_key_values_length, dtype=torch.long, device=device
+            # )
+            # position_ids = position_ids.unsqueeze(0)
 
         causal_mask = self._update_causal_mask(
             attention_mask, inputs_embeds, cache_position, past_key_values, output_attentions
@@ -85,6 +85,7 @@ class EmbedPreceedLlamaModel(LlamaModel):
 
         # create position embeddings to be shared across the decoder layers
         position_embeddings = self.rotary_emb(hidden_states, position_ids)
+        print(position_embeddings[0].shape, position_embeddings[1].shape, position_ids.shape)
 
         # decoder layers
         all_hidden_states = () if output_hidden_states else None
